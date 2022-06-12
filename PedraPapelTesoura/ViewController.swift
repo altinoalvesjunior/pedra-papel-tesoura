df5bed6f6345ec7faed17cb7c6ffe5ca35be8956
Altino Alves Júnior <altinoalvesjunior@gmail.com>
2022-06-12 11:08:55 -0300

    first commit

---
+++ PedraPapelTesoura/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PedraPapelTesoura
+//
+//  Created by Altino Alves Júnior on 11/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
