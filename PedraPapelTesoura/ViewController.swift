9b11d2199d7d05d7272f911d26470e065eea15d0
Altino Alves Júnior <altinoalvesjunior@gmail.com>
2022-06-12 15:33:39 -0300

    layout finished

--- PedraPapelTesoura/ViewController.swift
+++ PedraPapelTesoura/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var computerScoreLabel: UILabel!
+    @IBOutlet weak var meScoreLabel: UILabel!
+    @IBOutlet weak var computerImageView: UIImageView!
+    @IBOutlet weak var meImageView: UIImageView!
+    
+    @IBAction func playGame(_ sender: Any) {
+        let computerImageNumber = Int.random(in: 1...3)
+        let meImageNumber = Int.random(in: 1...3)
+        
+        setImage(imageView: computerImageView, number: computerImageNumber)
+        setImage(imageView: meImageView, number: meImageNumber)
+    }
+    
+    @IBAction func resetGame(_ sender: Any) {
+        computerScoreCount = 0
+        computerScoreLabel.text = String(computerScoreCount)
+        meScoreCount = 0
+        meScoreLabel.text = String(meScoreCount)
+    }
+    
+    var computerScoreCount = 0
+    var meScoreCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func setImage(imageView: UIImageView, number: Int) {
+        switch number {
+        case 1:
+            imageView.image = UIImage(named: "rock")
+        case 2:
+            imageView.image = UIImage(named: "paper")
+        case 3:
+            imageView.image = UIImage(named: "scissors")
+        default:
+            print("ERROR: Invalid Number!")
+        }
+    }
+    
+    func calculateScore(computerNumber: Int, meNumber: Int) {
+        
+    }
 }
 
